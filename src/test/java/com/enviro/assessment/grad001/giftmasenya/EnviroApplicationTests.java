d7947fb4d970544e4130654a72e6f9071b900b50
Senya- Man <masenyagift0@gmail.com>
2022-11-28 09:37:42 +0200

    init commit

---
+++ src/test/java/com/enviro/assessment/grad001/giftmasenya/EnviroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.giftmasenya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
