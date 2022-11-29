c52729f88809895ddc4465422250e030eb7470f6
Senya- Man <masenyagift0@gmail.com>
2022-11-29 10:33:07 +0200

    implemented the controller

--- src/main/java/com/enviro/assessment/grad001/giftmasenya/controller/ImageController.java
+++ src/main/java/com/enviro/assessment/grad001/giftmasenya/controller/ImageController.java
@@ -3,13 +3,19 @@ package com.enviro.assessment.grad001.giftmasenya.controller;
 import com.enviro.assessment.grad001.giftmasenya.service.FileParserService;
 import com.enviro.assessment.grad001.giftmasenya.utils.AppConstants;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,7 +27,26 @@ public class ImageController {
     @GetMapping(value = "/{name}/{surname}")
     public FileSystemResource getHttpImageLink(@PathVariable String name, @PathVariable String surname) throws IOException {
         fileParserService.parseCSV(AppConstants.FILE);
+        return new FileSystemResource(fileParserService.convertCSVDataToImage(imageData().get(0)));
+    }
 
-        return null;
+    private List<String> imageData() throws IOException {
+
+        List<String> imageData = new ArrayList<>();
+
+        Reader in = new FileReader(AppConstants.FILE);
+        Iterable<CSVRecord> records =  CSVFormat.DEFAULT
+                .builder()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .build()
+                .parse(in);
+
+        for(CSVRecord record : records) {
+            imageData = List.of(record.get("imageData"));
+
+        }
+
+        return imageData;
     }
 }
